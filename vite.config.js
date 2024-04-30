092d96cb95ec52e03ac55b9b67a905949cf91c3d
Jaxque <jackyyotsuba@gmail.com>
2024-04-30 12:56:10 -0400

    config

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
 })
